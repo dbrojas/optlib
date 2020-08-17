c2c66d2546aad8d7cc9ea216bec1c530c924ddb6
BartolomeD <d.bartolome.r@gmail.com>
2020-08-17 14:44:50 -0500

    Update setup description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="optlib",
     version="0.0.1",
-    description="A library to price financial options written in Python.",
+    description="A library for financial options pricing written in Python.",
     url="http://github.com/bartolomed/optlib",
     author="Davis Edwards & Daniel Rojas",
     packages=["optlib"],
